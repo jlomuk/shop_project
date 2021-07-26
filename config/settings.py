4979dd3b8d3300205a42edf2c400dd7406898b47
Денис Долгополов <dolgopolov@ssmp.local>
2021-07-26 10:41:32 +0300

    create new app 'smokeshop'

--- config/settings.py
+++ config/settings.py
@@ -16,6 +16,8 @@ DEBUG = env("DEBUG")
 ALLOWED_HOSTS = env('ALLOWED_HOSTS')
 
 INSTALLED_APPS = [
+    'smokeshop.apps.SmokeshopConfig',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
