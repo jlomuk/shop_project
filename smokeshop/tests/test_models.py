2a234fb4f30f5cdfa23536a09130cf3ab0a2ad1f
Денис Долгополов <dolgopolov@ssmp.local>
2021-07-27 15:33:25 +0300

    add new test for DetailView and model Product

--- smokeshop/tests/test_models.py
+++ smokeshop/tests/test_models.py
@@ -1,14 +1,21 @@
 from django.test import TestCase
 from django.urls import reverse
+from unittest import mock
+from django.core.files import File
 
-from smokeshop.models import Category
+from smokeshop.models import Category, Product
+
+
+file_mock = mock.MagicMock(spec=File, name='FileMock')
+file_mock.name = 'test1.jpg'
+file_mock.url = '/media/'
 
 
 class CategoryTest(TestCase):
-    """Тестирование добавленой логики в модель Категорий"""
+    """Тестирование добавленой логики в модель Category"""
 
     def setUp(self):
-        """Инициализация тестовых данных"""
+        """Инициализация тестовых данных (создание категории)"""
         self.category1 = Category.objects.create(
             name='CategoryTest1', slug='category_slug_1')
         self.category2 = Category.objects.create(
@@ -20,3 +27,23 @@ class CategoryTest(TestCase):
         calculate_url_2 = self.category2.get_absolute_url()
         self.assertEqual(calculate_url_1, '/category_slug_1/')
         self.assertEqual(calculate_url_2, '/category_slug_2/')
+
+
+class ProductTest(TestCase):
+    """Тестирование добавленой логики в модель Products"""
+
+    def setUp(self):
+        """Инициализация тестовых данных (создание категории и товара)"""
+        self.category1 = Category.objects.create(
+            name='CategoryTest1', slug='category_slug_1')
+        self.product1 = Product.objects.create(category=self.category1,
+                                               name='Product1', slug='product_slug_1',
+                                               is_nicotine=True, brand='Brand1',
+                                               maker_country='Banana', description='text',
+                                               weight=100, strength='1', smoking='2',
+                                               taste='apple', price=100, image=file_mock)
+
+    def test_urls_from_get_absolute_url(self):
+        """Проверка правильной генерации url'a через метод get_absolute_url"""
+        calculate_url = self.product1.get_absolute_url()
+        self.assertEqual(calculate_url, '/category_slug_1/product_slug_1/')
--- smokeshop/tests/test_views.py
+++ smokeshop/tests/test_views.py
@@ -60,9 +60,9 @@ class ProductListViewTest(TestCase):
             category__slug=self.category1.slug)
         products_by_category_2 = Product.objects.filter(
             category__slug=self.category2.slug)
-        self.assertQuerysetEqual(response_1.context['product_list'],
+        self.assertQuerysetEqual(response_1.context_data['product_list'],
                                  map(repr, products_by_category_1))
-        self.assertQuerysetEqual(response_2.context['product_list'],
+        self.assertQuerysetEqual(response_2.context_data['product_list'],
                                  map(repr, products_by_category_2))
 
     def test_response_with_products_by_nonexistent_slug(self):
@@ -72,5 +72,55 @@ class ProductListViewTest(TestCase):
                                            args=['nonexistent_slug']))
         products_by_nonexistent_slug = Product.objects.filter(
             category__slug='nonexistent_slug')
-        self.assertQuerysetEqual(response.context['product_list'],
+        self.assertQuerysetEqual(response.context_data['product_list'],
                                  map(repr, products_by_nonexistent_slug))
+
+
+class ProductDetailViewTest(TestCase):
+    """Набор тестов для проверки корректной работы обработчика, 
+    отвечающего за вывод детальной информации по товару"""
+
+    def setUp(self):
+        """Инициализация тестовых данных"""
+        self.category1 = Category.objects.create(
+            name='CategoryTest1', slug='category_slug_1')
+        self.category2 = Category.objects.create(
+            name='CategoryTest2', slug='category_slug_2')
+        self.product1 = Product.objects.create(category=self.category1,
+                                               name='Product1', slug='product_slug_1',
+                                               is_nicotine=True, brand='Brand1',
+                                               maker_country='^Banana', description='text',
+                                               weight=100, strength='1', smoking='2',
+                                               taste='apple', price=100, image=file_mock)
+        self.product2 = Product.objects.create(category=self.category2,
+                                               name='Product2', slug='product_slug_2',
+                                               is_nicotine=False, brand='Brand2',
+                                               maker_country='*NeBanana', description='text',
+                                               weight=50, strength='2', smoking='4',
+                                               taste='orange', price=200, image=file_mock)
+
+    def test_correct_use_template(self):
+        """Проверка вьюшки на использование правильного шаблона"""
+        response = self.client.get(reverse('smokeshop:product_detail',
+                                           args=[self.category2.slug, self.product2.slug]))
+        self.assertContains(response, '*NeBanana', status_code=200)
+        self.assertTemplateUsed(
+            response, 'smokeshop/product/detail.html')
+
+    def test_response_detail_product_success(self):
+        """Проверка передачи правильного продукта по слагу в контекст шаблона"""
+        response = self.client.get(reverse('smokeshop:product_detail',
+                                           args=[self.category1.slug, self.product1.slug]))
+        product = Product.objects.get(category__slug=self.category1.slug,
+                                      slug=self.product1.slug)
+        self.assertContains(response, '^Banana', status_code=200)
+        self.assertEqual(response.context_data['product'], product)
+
+    def test_response_detail_product_with_wrong_slug(self):
+        """Тест с неправильными слагами категории и продукта"""
+        response_1 = self.client.get(reverse('smokeshop:product_detail',
+                                             args=[self.category1.slug, 'wrong_slug']))
+        response_2 = self.client.get(reverse('smokeshop:product_detail',
+                                             args=['wrong_slug', self.product1.slug]))
+        self.assertEqual(response_1.status_code, 404)
+        self.assertEqual(response_2.status_code, 404)
