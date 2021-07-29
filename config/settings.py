e948b552726ad23df8234cf328f7139bd1a18343
Денис Долгополов <dolgopolov@ssmp.local>
2021-07-29 08:01:57 +0300

    initial new app cart

--- config/settings.py
+++ config/settings.py
@@ -101,3 +101,5 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CART_ID = 'cart'
--- smokeshop/mixins.py
+++ smokeshop/mixins.py
@@ -1,6 +1,9 @@
 class CategoryFilterToSlugMixin:
 
     def get_queryset(self, *args, **kwargs):
+        """
+        Миксин для фильтрации товара по конкретному slug'у категории товара
+        """
         qs = self.queryset
         if self.kwargs.get('category_slug'):
             qs = qs.filter(category__slug=self.kwargs['category_slug'])
