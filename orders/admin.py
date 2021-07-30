5068da68039219adc17d54200fe79a6b15b9ff3d
Денис Долгополов <dolgopolov@ssmp.local>
2021-07-30 09:16:44 +0300

    register models orders app to adminsite, repair template cart

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Order, OrderItem
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'first_name', 'last_name',
+        'email', 'address', 'city',
+        'transport', 'status', 'created'
+    )
+    list_filter = ('created',)
+    list_editable = ('status',)
+    search_fields = ('=first_name', '=last_name', '=id')
+    inlines = (OrderItemInline,)
--- orders/models.py
+++ orders/models.py
@@ -52,14 +52,18 @@ class Order(models.Model):
         blank=True, verbose_name='Примечания'
     )
     transport = models.CharField(
-        max_length=20, choices=TRANSPORT
+        max_length=20, choices=TRANSPORT,
+        verbose_name='Способ получения'
     )
     transport_cost = models.DecimalField(
-        max_digits=8, decimal_places=2
+        max_digits=8, decimal_places=2,
+        verbose_name='Цена доставки'
     )
 
     class Meta:
         ordering = ('-created',)
+        verbose_name_plural = 'Заказы'
+        verbose_name = 'Заказ'
 
     def __str__(self):
         return f'Заказ № {self.id}'
@@ -75,22 +79,30 @@ class OrderItem(models.Model):
     """Модель для связи продукта и его количества с конкретным заказом"""
     order = models.ForeignKey(
         Order, related_name='items',
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
     )
     product = models.ForeignKey(
         Product, related_name='order_items',
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        verbose_name='Товары'
     )
     price = models.DecimalField(
-        max_digits=10, decimal_places=2
+        max_digits=10, decimal_places=2,
+        verbose_name='Цена'
     )
-    quantity = models.PositiveIntegerField()
+    quantity = models.PositiveIntegerField(
+        verbose_name='Количество'
+    )
 
     def __str__(self):
         return str(self.id)
 
     def get_cost(self):
         return self.price * self.quantity
+
+    class Meta:
+        verbose_name_plural = 'Товары в заказе'
+        verbose_name = 'Товар в заказ'
 
 
 
