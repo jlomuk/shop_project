a1199aa9f27c56411b6c531e27f4d9c6d005b0d5
Денис Долгополов <dolgopolov@ssmp.local>
2021-08-03 08:39:04 +0300

    create xlsx report for orders

--- orders/admin.py
+++ orders/admin.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
 
 from .models import Order, OrderItem
+from smokeshop.models import Product
+from .service import initial_response_for_xlsx, create_report_to_xlsx_for_orders
 
 
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
+
+
+def export_to_xlsx(modeladmin, request, queryset):
+    """Формирования отчета по заказам в админке"""
+    opts = modeladmin.model._meta
+    products = Product.objects.all()
+    response = initial_response_for_xlsx()
+    return create_report_to_xlsx_for_orders(response, opts, products, queryset)
+export_to_xlsx.short_description = 'Export to XLSX'
 
 
 @admin.register(Order)
@@ -18,3 +29,4 @@ class OrderAdmin(admin.ModelAdmin):
     list_editable = ('status',)
     search_fields = ('=first_name', '=last_name', '=id')
     inlines = (OrderItemInline,)
+    actions = (export_to_xlsx, )
--- orders/service.py
+++ orders/service.py
@@ -1,13 +1,70 @@
-from decimal import Decimal
 import stripe
+import datetime
+import xlsxwriter
+from decimal import Decimal
 
 from django.conf import settings
+from django.http import HttpResponse
 
 from .models import OrderItem
 from cart.services.cart import Cart
 
 
 stripe.api_key = settings.STRIPE_TEST_SECRET_KEY
+
+
+def initial_response_for_xlsx():
+    """Создания ответа для передачи созданого xlsx"""
+    timestamp = datetime.datetime.now().strftime('%d-%m-%Y')
+    content_disposition = f'attachment; filename=orders_{timestamp}.xlsx'
+    response = HttpResponse(
+        content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+    response['Content-Disposition'] = content_disposition
+    return response
+
+
+def create_report_to_xlsx_for_orders(response, opts, products, queryset):
+    """формирование xlsx отчета по заказам"""
+    workbook = xlsxwriter.Workbook(response, {'in_memory': True})
+    worksheet = workbook.add_worksheet()
+    fields = [field for field in opts.get_fields() if not field.one_to_many]
+    header_list = [field.name for field in fields]
+    for product in products:
+        header_list.append(f'{product.name} qty')
+        header_list.append(f'{product.name} price')
+
+    header_list.append('order_price_total')
+    for column, item in enumerate(header_list):
+        worksheet.write(0, column, item)
+
+    for row, obj in enumerate(queryset):
+        prod_tracker = {product.name: {'qty': 0, 'price':0} for product in products}
+        order_items = obj.items.all()
+        for item in order_items:
+            prod_tracker[item.product.name]['qty'] = item.quantity
+            prod_tracker[item.product.name]['price'] = item.price
+        data_row = []
+        order_price_total = 0
+        for field in fields:
+            value = getattr(obj, field.name)
+            if field.name == 'transport_cost':
+                order_price_total += value 
+            if isinstance(value, datetime.datetime):
+                value = value.strftime('%d/%m/%Y')
+            data_row.append(value)
+
+        for product in prod_tracker:
+            for qty_price in prod_tracker[product]:
+                data_row.append(prod_tracker[product][qty_price])
+            order_price_total += (
+                prod_tracker[product]['qty'] *  prod_tracker[product]['price']
+            )
+        data_row.append(order_price_total)
+        for column, item in enumerate(data_row):
+            worksheet.write(row + 1, column, item)
+    workbook.close()
+    return response
+    
 
 
 def _get_customer(request, order):
