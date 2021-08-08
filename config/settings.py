ac71e9486c5dc8a653a5c68e9782200d42b340f2
Denis Dolgopolov <jlomuk1990@gmail.com>
2021-08-08 15:01:02 +0300

    modify settings

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,7 @@ environ.Env.read_env(env_file=os.path.join(BASE_DIR, '.env'))
 
 SECRET_KEY = env("SECRET_KEY")
 
-DEBUG = env("DEBUG")
+DEBUG = int(env("DEBUG"))
 
 ALLOWED_HOSTS = env('ALLOWED_HOSTS')
 
